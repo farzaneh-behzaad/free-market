c2ffa619d6ab4371cb1f42708b98970999a91a7a
Farzaneh Behzaad <farzaneh.behzaad@gmail.com>
2020-05-10 15:29:09 +0430

    chore: remove callback on networking and use live data

--- app/src/main/java/pro/vteam/freemarket/repository/Repository.java
+++ app/src/main/java/pro/vteam/freemarket/repository/Repository.java
@@ -3,8 +3,13 @@ package pro.vteam.freemarket.repository;
 import android.util.Log;
 
 
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+
 import pro.vteam.freemarket.interfaces.CategoriesService;
 
+import pro.vteam.freemarket.models.CategoriesList;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,25 +24,25 @@ public class Repository {
     }
 
 
-    public static void getCategoriesLists(CategoryFetchListener listener) {
+    public static void getCategoriesLists(MutableLiveData<ArrayList<CategoriesList>> liveData) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://raw.githubusercontent.com/m-khademloo/free-market-mocks/master/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-         CategoriesService categoriesService = retrofit.create(CategoriesService.class);
+        CategoriesService categoriesService = retrofit.create(CategoriesService.class);
 
         Call<pro.vteam.freemarket.models.Response> call = categoriesService.getResponse();
+
         call.enqueue(new Callback<pro.vteam.freemarket.models.Response>() {
             @Override
             public void onResponse(Call<pro.vteam.freemarket.models.Response> call, Response<pro.vteam.freemarket.models.Response> response) {
-                listener.onSuccess(response.body());
+                liveData.setValue(response.body().getResult().getCategoriesLists());
             }
 
             @Override
             public void onFailure(Call<pro.vteam.freemarket.models.Response> call, Throwable t) {
                 Log.i("LOG","failure");
-                listener.onFailed();
             }
         });
 
--- app/src/main/java/pro/vteam/freemarket/ui/categories/CategoriesTabViewModel.java
+++ app/src/main/java/pro/vteam/freemarket/ui/categories/CategoriesTabViewModel.java
@@ -6,8 +6,6 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 
 import pro.vteam.freemarket.models.CategoriesList;
-import pro.vteam.freemarket.models.CategoriesListItems;
-import pro.vteam.freemarket.models.Response;
 import pro.vteam.freemarket.repository.Repository;
 
 public class CategoriesTabViewModel extends ViewModel {
@@ -16,50 +14,11 @@ public class CategoriesTabViewModel extends ViewModel {
 
     public CategoriesTabViewModel() {
         lists = new MutableLiveData<>();
-
-
-        Repository.getCategoriesLists(new Repository.CategoryFetchListener() {
-            @Override
-            public void onSuccess(Response body) {
-                lists.setValue(body.getResult().getCategoriesLists());
-
-            }
-
-            @Override
-            public void onFailed() {
-
-            }
-        });
-
-
-
+        Repository.getCategoriesLists(lists);
     }
 
     public MutableLiveData<ArrayList<CategoriesList>> getObjectsList() {
         return lists;
     }
-
-
-
-//
-//        ArrayList<CategoriesListItems> programsList = new ArrayList<>();
-//        for (int i = 1; i <= 20; i++) {
-//            CategoriesListItems object = new CategoriesListItems("برنامه " + i, "@drawable/ic_edit_black_24dp");
-//            programsList.add(object);
-//        }
-//
-//        ArrayList<CategoriesListItems> gamesList = new ArrayList<>();
-//        for (int i = 1; i <= 20; i++) {
-//            CategoriesListItems object = new CategoriesListItems("بازی " + i, "@drawable/ic_edit_black_24dp");
-//            gamesList.add(object);
-//        }
-//
-//        ArrayList<CategoriesList> list = new ArrayList<>();
-//        list.add(new CategoriesList("بازی ها ", gamesList));
-//        list.add(new CategoriesList("برنامه ها ", programsList));
-//
-//        return list;
-//    }
-
 
 }
