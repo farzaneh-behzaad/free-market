a74698709bf5de41011122fb09a1e51d2e7c43ac
Farzaneh Behzaad <farzaneh.behzaad@gmail.com>
2020-05-18 16:46:45 +0430

    feat : add switch button to MyMarket list and fix som margin

--- app/src/main/java/pro/vteam/freemarket/adapter/CategoriesPagerAdapter.java
+++ app/src/main/java/pro/vteam/freemarket/adapter/CategoriesPagerAdapter.java
@@ -16,22 +16,42 @@ public class CategoriesPagerAdapter extends FragmentPagerAdapter {
     public CategoriesPagerAdapter(FragmentManager fm, ArrayList<CategoriesList> listModels) {
         super(fm);
         this.listModels = listModels;
+
     }
 
     @Override
     public Fragment getItem(int position) {
-        return new ProgramsGamesFragment(listModels.get(position).getListItems());
+
+
+       // return new ProgramsGamesFragment(listModels.get(position).getListItems());
+
+        // because of change tabs order
+        if(position==0){
+         return new ProgramsGamesFragment(listModels.get(1).getListItems());
+        }
+        else {
+            return new ProgramsGamesFragment(listModels.get(0).getListItems());
+        }
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return listModels.get(position).getTitle();
+
+      //  return listModels.get(position).getTitle();
+
+      // because of change tabs order
+        if(position==0){
+            return listModels.get(1).getTitle();}
+        else {
+            return    listModels.get(0).getTitle();}
     }
 
     @Override
     public int getCount() {
         return listModels.size();
     }
+
+
 
 
 }
