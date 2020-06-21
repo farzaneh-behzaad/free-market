1162b8deb1f07e3c0a03e8527b68198159ec3a77
Farzaneh Behzaad <farzaneh.behzaad@gmail.com>
2020-06-21 18:34:54 +0430

    feat: add tag for banners in oneRowBanners

--- app/src/main/java/pro/vteam/freemarket/adapter/BannerAdapter.java
+++ app/src/main/java/pro/vteam/freemarket/adapter/BannerAdapter.java
@@ -1,10 +1,12 @@
 package pro.vteam.freemarket.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -38,6 +40,7 @@ public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.viewHolder
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.model_banner,parent,false);
         BannerAdapter.viewHolder viewHolder= new viewHolder(view);
         viewHolder.bannerImage =view.findViewById(R.id.img_banner);
+        viewHolder.txt_bannerTag =view.findViewById(R.id.txt_bannerTag);
 
         return viewHolder;
     }
@@ -46,6 +49,15 @@ public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.viewHolder
     public void onBindViewHolder(@NonNull viewHolder holder, int position) {
 
         HomeBanner item = list.get(position);
+
+        if(item.getHomeTag() == null){
+            holder.txt_bannerTag.setVisibility(View.GONE);
+        }
+        else {
+            holder.txt_bannerTag.setText(item.getHomeTag().getTitle());
+            holder.txt_bannerTag.setBackgroundColor(Color.parseColor(item.getHomeTag().getBackgroundColor()));
+
+        }
 
         ((ConstraintLayout.LayoutParams) holder.bannerImage.getLayoutParams()).dimensionRatio
                 =item.getHomeImage().getRatio().getConstraintDimensionRatio();
@@ -66,11 +78,14 @@ public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.viewHolder
     static class viewHolder extends RecyclerView.ViewHolder{
 
         private ImageView bannerImage;
+        private TextView txt_bannerTag;
+
 
 
         viewHolder(@NonNull View itemView) {
             super(itemView);
             bannerImage = itemView.findViewById(R.id.img_banner);
+            txt_bannerTag = itemView.findViewById(R.id.txt_bannerTag);
 
         }
     }
